f3c242c0926b4e172462dc839db387d50df2275c
ValerieAvva <valerie.avva@gmail.com>
2016-09-06 22:44:11 -0400

    did my method and updated the antlabmain

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -54,11 +54,10 @@ import edu.gatech.oad.antlab.pkg2.*;
         
         String toPrint = 
             ant11.getMessage() + ant12.getMessage() + ant21.getMessage()
-          + ant22.getMessage() + ant31.getMessage();
-		  //Person1 replace P1 with your name
+          + ant22.getMessage() + ant31.getMessage();						  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("P1");
-		  toPrint += p1.toString("gburdell1");
+		  Person1 p1 = new Person1("Valerie Avva");
+		  toPrint += p1.toString("vavva3");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("P2");
