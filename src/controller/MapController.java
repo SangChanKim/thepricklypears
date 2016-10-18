0a5b9a68142702d427b4dd98c2b0dbbaab2bc5d7
vchen36 <vchen36@gatech.edu>
2016-10-18 17:26:06 -0400

    Started doing some UI for map and controller stuff

---
+++ src/controller/MapController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import fxapp.MainFXApplication;
+import javafx.fxml.FXML;
+
+/**
+ * Created by Victor on 10/18/2016.
+ */
+public class MapController {
+
+    private MainFXApplication mainFXApplication;
+
+    private GoogleMap map;
+
+    /*  **********************
+            References to the FXML widgets in the .fxml file
+        */
+    @FXML
+    private GoogleMapView mapView;
+
+
+
+}
